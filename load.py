e0a12758ef4e70484adf3a1507a4c664744eb345
Uma-Mahe <uma6mahe@gmail.com>
2022-03-09 22:57:37 -0600

    adding all the files

---
+++ load.py
@@ -0,0 +1,24 @@
+#Load the files to data frame
+
+from pyspark.sql import SparkSession
+
+spark=SparkSession.builder\
+        .appName("Load")\
+        .getOrCreate()
+
+ordersDF=spark.read.csv("gs://mybucket38/salesorder/orders.csv", inferSchema=True, header=True)
+productsDF=spark.read.csv("gs://mybucket38/salesorder/products.txt", inferSchema=True, header=True)
+customersDF=spark.read.csv("gs://mybucket38/salesorder/customers.txt", inferSchema=True, header=True)
+categoriesDF=spark.read.csv("gs://mybucket38/salesorder/categories.txt", inferSchema=True, header=True)
+salesDF=spark.read.csv("gs://mybucket38/salesorder/Sales.csv", inferSchema=True, header=True)
+
+
+"""ordersDF.show(2)
+productsDF.show(2)
+customersDF.show(2)
+categoriesDF.show(2)
+salesDF.show(2)
+"""
+
+spark-submit writeinhive.py
+
